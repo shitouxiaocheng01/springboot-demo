47671e3e11745ac462b630afb6cfcafcbea52996
凌毅 <781293247@qq.com>
2019-12-09 17:15:12 +0800

    SpringBoot(五)：整合mybatis-plus

---
+++ spring-boot-mybatisplus/src/test/java/top/xluo/mybatisplus/SpringBootMybatisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xluo.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
