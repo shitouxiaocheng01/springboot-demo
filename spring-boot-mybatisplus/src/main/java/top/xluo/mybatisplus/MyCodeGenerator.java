8bf8eab3231bfc6d590719e1d47e3ae0941fb883
凌毅 <781293247@qq.com>
2019-12-19 17:32:16 +0800

    SpringBoot(五)：整合mybatis-plus

--- spring-boot-mybatisplus/src/main/java/top/xluo/mybatisplus/MyCodeGenerator.java
+++ spring-boot-mybatisplus/src/main/java/top/xluo/mybatisplus/MyCodeGenerator.java
@@ -4,9 +4,12 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.IColumnType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 /**
  * @Author Lingyi
@@ -28,6 +31,7 @@ public class MyCodeGenerator {
                     .setEnableCache(false)
                     .setBaseResultMap(true)
                     .setBaseColumnList(true)
+                    .setSwagger2(true)
                     // 自定义文件命名, %s 会自动填充表实体属性
                     .setEntityName("%sEntity")
                     .setMapperName("%sMapper")
@@ -35,6 +39,7 @@ public class MyCodeGenerator {
                     .setServiceName("%sService")
                     .setControllerName("%sController")
         ).setDataSource(
+                // 数据源配置
                 new DataSourceConfig()
                     .setDbType(DbType.MYSQL)
                     .setTypeConvert(new MySqlTypeConvert() {
@@ -51,6 +56,30 @@ public class MyCodeGenerator {
                     .setUsername("dbAdminC")
                     .setPassword("Kenuo123")
                     .setUrl("jdbc:mysql://10.122.81.99:3306/dawn?useUnicode=true&characterEncoding=utf-8&useSSL=false")
+        ).setStrategy(
+                // 策略配置
+                new StrategyConfig()
+//                    .setCapitalMode(true)   //oracle可以开启
+//                    .setTablePrefix(new String[] {"user"})   //修改表前缀
+                    .setNaming(NamingStrategy.no_change)
+                    .setInclude(new String[] {"person"})    // 需要生成的表
+//                    .setExclude(new String[] {"test"})    // 排除生成的表
+//                    .setSuperEntityClass("com.xx.xx.XxEntity")    // 自定义实体父类
+                    .setSuperEntityColumns(new String[] {"class_id"})    // 自定义公共字段
+                    .setSuperMapperClass("com.xx.xx.XxMapper")
+                    .setSuperServiceClass("")
+                    .setSuperServiceImplClass("")
+                    .setEntityLombokModel(true)
+                    .setEntityBooleanColumnRemoveIsPrefix(true)
+                    .setRestControllerStyle(true)
+                    .setControllerMappingHyphenStyle(true)
+                    .setEntityTableFieldAnnotationEnable(true)
+        ).setPackageInfo(
+                // 包配置
+                new PackageConfig()
+                    .setParent("top.xluo.mybatisplus")
+                    .setController("controller")  // 默认web
         );
+        mpg.execute();
     }
 }
